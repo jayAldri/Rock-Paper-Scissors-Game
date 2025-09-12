001b0996cf1e6000c38d10d78533f6a27e123d69
Jay Aldrin <jayaldrinorabi015@gmail.com>
2025-09-12 21:14:23 +0800

    updated

--- script.js
+++ script.js
@@ -71,8 +71,6 @@
       document.querySelector('.js-result').innerHTML = result;
       document.querySelector('.js-moves').innerHTML = `You <img src="images/${playerMove}-emoji.png" class="move-ico">
       - Computer Pick <img src="images/${computerMove}-emoji.png" class="move-ico">`;
-
-
     }
 
 
