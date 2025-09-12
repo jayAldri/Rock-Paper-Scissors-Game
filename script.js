c8439f857e4f487d902c24c4f82c4647bcb4dc2b
Jay Aldrin <jayaldrinorabi015@gmail.com>
2025-09-12 22:17:15 +0800

    updated w/ image displayed

--- script.js
+++ script.js
@@ -15,11 +15,11 @@
       let computerMove = '';
 
       if (randomNumber >= 0 && randomNumber < 1/3) {
-        computerMove = 'Rock';
+        computerMove = 'rock';
       } else if (randomNumber >= 1/3 && randomNumber < 2/3) {
-        computerMove = 'Paper';
+        computerMove = 'paper';
       } else if (randomNumber >= 2/3 && randomNumber < 1) {
-        computerMove = 'Scissors';
+        computerMove = 'scissors';
       }
 
       return computerMove;
@@ -30,28 +30,28 @@
 
       let result = '';
 
-       if (playerMove === 'Scissors') {
-        if (computerMove === 'Rock') {
+       if (playerMove === 'scissors') {
+        if (computerMove === 'rock') {
           result = 'You lose!';
-        } else if (computerMove === 'Paper') {
+        } else if (computerMove === 'paper') {
           result = 'You win!';
-        } else if (computerMove === 'Scissors') {
+        } else if (computerMove === 'scissors') {
           result = 'Tie!';
         }
-      } else if (playerMove === 'Rock') {
-        if (computerMove === 'Rock') {
+      } else if (playerMove === 'rock') {
+        if (computerMove === 'rock') {
           result = 'Tie!';
-        } else if (computerMove === 'Paper') {
+        } else if (computerMove === 'paper') {
           result = 'You lose!';
-        } else if (computerMove === 'Scissors') {
+        } else if (computerMove === 'scissors') {
           result = 'You win!';
         }
-      } else if (playerMove === 'Paper') {
-        if (computerMove === 'Rock') {
+      } else if (playerMove === 'paper') {
+        if (computerMove === 'rock') {
           result = 'You win!';
-        } else if (computerMove === 'Paper') {
+        } else if (computerMove === 'paper') {
           result = 'Tie!';
-        } else if (computerMove === 'Scissors') {
+        } else if (computerMove === 'scissors') {
           result = 'You lose!';
         }
       }
